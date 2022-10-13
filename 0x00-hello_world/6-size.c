020eba5eb97b5996c517d5bd06932afd3d80f072
Ejiro-IO <ejiroighorodje18@gmail.com>
2022-10-13 10:56:31 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %i byte(s)\n", 1'byte');
-	printf("Size of an int: %i byte(s)\n", 4'bytes');
-	printf("Size of a long int: %i byte(s)\n", 4'bytes');
-	printf("Size of a long long int: %i byte(s)\n", 8'bytes');
-	printf("Size of a float: %i byte(s)\n", 4'bytes');
+	printf("Size of a char: %i byte(s)\n", 1);
+	printf("Size of an int: %i byte(s)\n", 4);
+	printf("Size of a long int: %i byte(s)\n", 4);
+	printf("Size of a long long int: %i byte(s)\n", 8);
+	printf("Size of a float: %i byte(s)\n", 4);
 	return (0);
 }
