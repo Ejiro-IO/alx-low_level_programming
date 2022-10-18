1d5f99e69b9abf266b416e4044593386337087e7
Ejiro-IO <ejiroighorodje18@gmail.com>
2022-10-18 16:04:02 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @o: first number input
+ * @t: second number input
+ * Return: o + t
+ */
+int add(int o, int t)
+{
+	return (o + t);
+}
