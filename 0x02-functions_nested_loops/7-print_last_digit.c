56989f8ecb95c62f6beda7bd6c4a9a28604ed2ce
Ejiro-IO <ejiroighorodje18@gmail.com>
2022-10-18 14:20:14 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,15 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10
+	int ld;
 
-		if (ld < 0)
-			ld *= -1;
+	ld = (n % 10);
+
+	if (ld < 0)
+	{
+		ld = (-1 * ld);
+	}
+
 	_putchar(ld + '0');
-
 	return (0);
 }
