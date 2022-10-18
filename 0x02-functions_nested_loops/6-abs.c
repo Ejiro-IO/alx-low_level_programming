fc590c7c4e33dbcb5242f2d5d3849e630b0ff93d
Ejiro-IO <ejiroighorodje18@gmail.com>
2022-10-18 13:57:03 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - computes theabsolute value of an integer
+ * @n: integer input
+ * Return: absolute value of ab
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
